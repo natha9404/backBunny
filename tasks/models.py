e22d8c13cbc9427779662a9424ab13aa52b13e04
Nathalia Bedoya <nathalia.bedoya@grupossi.com>
2020-11-27 18:09:19 -0500

    CRUD for users and tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from users.models import Users
+from users.models import OwnUser
 
 alpha = RegexValidator(r'^[a-zA-Z]*$', 'Only alpha')
 
@@ -12,8 +12,8 @@ class User_Tasks (models.Model):
     description = models.CharField(
         max_length=500, verbose_name="Description", blank=True, validators=[alpha])
     state = models.CharField(max_length=20, verbose_name="State", default='To Do', choices=state_choices)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(OwnUser, on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return '%s %s' % (self.description)
+        return self.description
--- users/models.py
+++ users/models.py
@@ -5,8 +5,9 @@ from django.core.validators import RegexValidator
 alpha = RegexValidator(r'^[a-zA-Z]*$', 'Only alpha')
 
 # Create your models here.
-class Users (User):
-    name = models.CharField(max_length=100, verbose_name="Name", blank=True, validators=[alpha])
+class OwnUser (models.Model):
+    name = models.CharField(
+        max_length=100, verbose_name="Name User", blank=True, validators=[alpha])
 
     def __str__(self):
-        return '%s %s' % (self.name)
+        return self.name
